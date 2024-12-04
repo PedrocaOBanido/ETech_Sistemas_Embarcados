8eff01e07e77b970b4c2d8b43863ce6750f4f6ff
PedrocaOBanido <pedroernestopgoncalves@gmail.com>
2024-12-04 00:41:01 -0400

    last commit
    comunicação entre arduino com sensores dht e ldr enviando telemetria para esp32 por protocolo serial
    
    e motor dc e relay ligado no circuito para quando o comando dado via mqtt fosse executado em app, e sinal enviado pelo esp32

--- Projeto Final/src/main.cpp
+++ Projeto Final/src/main.cpp
@@ -1,20 +1,146 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 #include <SoftwareSerial.h>
 
-const int rx_pin = 8;
-const int tx_pin = 9;
+const int RX_PIN = 9;
+const int TX_PIN = 8;
+const int LDR_PIN = A3;
+const int DHT_PIN = 4;
+const int RELAY_PIN = 6;
+const int MOTOR_PIN = 3;
 
-SoftwareSerial serial_esp (rx_pin, tx_pin);
+uint8_t pwm = 0;
+bool motor_state = false;
+
+SoftwareSerial new_serial(RX_PIN, TX_PIN);
+
+DHT dht(DHT_PIN, DHT11);
+
+uint16_t readLDR(){
+  return analogRead(LDR_PIN);
+}
+
+void controlRelay(bool state){
+  digitalWrite(RELAY_PIN, state);
+}
+
+void controlMotor(){
+  if (motor_state){
+    analogWrite(MOTOR_PIN, pwm);
+  }
+  else {
+    digitalWrite(MOTOR_PIN, LOW);
+  }
+}
+
+float readTemp(){
+  return dht.readTemperature();
+}
+
+float readHum(){
+  return dht.readHumidity();
+}
+
+void clearSerial(){
+  while (new_serial.available()>0){
+    new_serial.read();
+  }
+}
 
 void setup() {
   Serial.begin(9600);
-  serial_esp.begin(9600);
+  new_serial.begin(9600);
+  pinMode(LDR_PIN, INPUT);
+  pinMode(DHT_PIN, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(MOTOR_PIN, OUTPUT);
+  dht.begin();
 }
 
-void loop() {
-  if(serial_esp.available() > 0){
-    String message = serial_esp.readStringUntil('\n');
-    Serial.println(message);
+void loop()
+{
+  if (new_serial.available() > 0)
+  {
+    delay(10);
+    String command = new_serial.readStringUntil('\n');
+    Serial.println("Command received " + command);
+    if (command.startsWith("VELOCITY:"))
+    {
+      int received_pwd = command.substring(9).toInt();
+      if (received_pwd >= 0 && received_pwd <= 255)
+      {
+        pwm = received_pwd;
+        if (motor_state == true)
+        {
+          controlMotor();
+        }
+        Serial.println("Motor PWM: " + String(pwm));
+      }
+      else
+      {
+        Serial.println("Invalid PWM value");
+      }
+    }
+    else if (command.startsWith("MOTOR:"))
+    {
+      int received_state = command.substring(6).toInt();
+      if (received_state == 0)
+      {
+        motor_state = false;
+        controlMotor();
+      }
+      else if (received_state == 1)
+      {
+        motor_state = true;
+        controlMotor();
+      }
+      else
+      {
+        Serial.println("Invalid motor value");
+      }
+    }
+    else if (command.startsWith("RELAY:"))
+    {
+      int state = command.substring(6).toInt();
+      if (state == 0 || state == 1)
+      {
+        controlRelay(state);
+        Serial.println("Relay state: " + String(state));
+      }
+      else
+      {
+        Serial.println("Invalid relay state");
+      }
+    }
+    else
+    {
+      Serial.println("Invalid command");
+    }
+    clearSerial();
   }
-  delay(10);
+
+  uint16_t ldr_value = readLDR();
+  float temp = readTemp();
+  float hum = readHum();
+
+  if (isnan(temp) || isnan(hum))
+  {
+    Serial.println("Error to read DHT11");
+  }
+  else
+  {
+    new_serial.print(ldr_value);
+    new_serial.print("-");
+    new_serial.print(temp, 2);
+    new_serial.print("-");
+    new_serial.println(hum, 2);
+    Serial.print("LDR: ");
+    Serial.println(ldr_value);
+    Serial.print("Temperatura: ");
+    Serial.println(temp);
+    Serial.print("Umidade: ");
+    Serial.println(hum);
+  }
+  delay(500);
 }
--- projeto final esp32/src/main.cpp
+++ projeto final esp32/src/main.cpp
@@ -1,18 +1,132 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
-bool ledState = false;
+const char* WIFI_SSID = "Etech";
+const char* WIFI_PASSWORD = "al40030f";
 
+const char* MQTT_BROKER = "10.31.3.108"; 
+const int MQTT_PORT = 1883;
+const char* CLIENT_ID = "ESP32_Device_pedro";
+
+// Publish topics
+const char* TEMPERATURE_TOPIC = "/pedro/temperatura";
+const char* HUMIDITY_TOPIC = "/pedro/umidade";
+const char* LDR_TOPIC = "/pedro/ldr";
+
+/// Subscribe topics
+const char* RELAY_TOPIC = "/pedro/rele";
+const char* MOTOR_TOPIC = "pedroraphael/motor";
+const char* VELOCITY_TOPIC = "/pedro/velocidade";
+
+const int LED_PIN = 2;
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
+void connectToWiFi() {
+    Serial.println("Connecting to WiFi...");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    bool led_state = true;
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        digitalWrite(LED_PIN, led_state);
+        led_state = !led_state;
+    }
+    digitalWrite(LED_PIN, HIGH);
+
+    Serial.println("\nWiFi Connected!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void checkWiFiConnection() {
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("WiFi disconnected, attempting to reconnect...");
+        connectToWiFi();
+    }
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Message received on topic: ");
+    Serial.println(topic);
+    
+    String message = "";
+    for (unsigned int i = 0; i < length; i++) {
+        message += (char)payload[i];
+    }
+
+    if (String(topic) == MOTOR_TOPIC) {
+        Serial2.println("MOTOR:" + message);
+    } else if (String(topic) == VELOCITY_TOPIC) {
+        Serial2.println("VELOCITY:" + message);
+    } else if (String(topic) == RELAY_TOPIC) {
+        Serial2.println("RELAY:" + message);
+    }
+}
+
+void reconnectMQTT() {
+    while (!mqttClient.connected()) {
+        Serial.println("Attempting MQTT Connection...");
+        
+        if (mqttClient.connect(CLIENT_ID)) {
+            Serial.println("MQTT Connected!");
+            mqttClient.subscribe(RELAY_TOPIC);
+            mqttClient.subscribe(MOTOR_TOPIC);
+            mqttClient.subscribe(VELOCITY_TOPIC);
+        } else {
+            Serial.print("Failed, rc=");
+            Serial.print(mqttClient.state());
+            Serial.println(" Retrying in 5 seconds");
+            delay(5000);
+        }
+    }
+}
 
 void setup() {
-  Serial.begin(115200);
-  Serial2.begin(9600);
-  pinMode(2, OUTPUT);
+    Serial.begin(115200);
+    Serial2.begin(9600);
+    pinMode(LED_PIN, OUTPUT);
+    connectToWiFi();
+    mqttClient.setServer(MQTT_BROKER, MQTT_PORT);
+    mqttClient.setCallback(mqttCallback);
 }
 
 void loop() {
-  Serial2.println(random(1, 10));
-  digitalWrite(2, HIGH);
-  delay(100);
-  digitalWrite(2, LOW);
-  delay(1000);
+
+  checkWiFiConnection();
+
+  if (!mqttClient.connected()) {
+      reconnectMQTT();
+  }
+  mqttClient.loop();
+  if (Serial2.available() > 0) {
+    String message = Serial2.readStringUntil('\n');
+    int index_1 = message.indexOf('-');
+    if (index_1 > 0) {
+        uint16_t ldr_value = message.substring(0, index_1).toInt();
+        int index_2 = message.indexOf('-', index_1 + 1);
+        if (index_2 > 0) {
+            float temperature = message.substring(index_1 + 1, index_2).toFloat();
+            float humidity = message.substring(index_2 + 1).toFloat();
+            Serial.print("LDR: ");
+            Serial.println(ldr_value);
+            Serial.print("Temperature: ");
+            Serial.println(temperature);
+            Serial.print("Humidity: ");
+            Serial.println(humidity);
+
+            mqttClient.publish(LDR_TOPIC, String(ldr_value).c_str());
+            mqttClient.publish(TEMPERATURE_TOPIC, String(temperature, 2).c_str());
+            mqttClient.publish(HUMIDITY_TOPIC, String(humidity, 2).c_str());
+        } else {
+            Serial.println("Error: invalid message format");
+        }
+    } else {
+        Serial.println("Error: invalid message format available");
+    }
+  }  
+  delay(10);
 }
+
+// lib_deps = knolleary/PubSubClient@^2.8
